3b74682a52fc8a8094a2b4b3c2e4ea1d7afba396
Dynaflow-git <creatives@dynaflowph.com>
2025-01-14 16:41:04 +0800

    initial commit

---
+++ con.php
@@ -0,0 +1,17 @@
+<php?
+
+ $username='';
+ $password='*';
+ $db_name='';
+ $localhost='';
+
+ $con = ();
+
+ if $con = not equal {
+    print 'error connecting';
+ } else {
+    include_once " index.php";
+ }
+ 
+ 
+ 
